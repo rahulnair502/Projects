faefe1cec0d409aacd8901404b4b7ad1b7b5db18
rahulnair502 <71089258+rahulnair502@users.noreply.github.com>
2022-01-08 16:58:18 -0500

    Create public_account.py

---
+++ Instagram_Web_Scraper/public_account.py
@@ -0,0 +1,9 @@
+##used for accounts that are not yours
+class public_account():
+    def __init__(self, username):
+        self.username = username
+
+    def insta_page2(self):
+        print(f"https://www.instagram.com/{self.username}/?hl=en")
+        driver.get(f"https://www.instagram.com/{self.username}/?hl=en")
+        time.sleep(random.uniform(.75, 1.5))
